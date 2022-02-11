42d11e932853330ddf2fc42f5c21d94ed386ad0b
Murali Singamsetty <kumarsm@users.noreply.github.com>
2022-02-11 11:44:58 -0800

    Updated for Wordle Challenge

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -15,52 +15,36 @@ export const InfoModal = ({ isOpen, handleClose, darkMode, colorBlindMode, style
       </button>
       <div className="h-full flex flex-col items-center justify-center max-w-[390px] mx-auto pt-9 text-primary dark:text-primary-dark">
         <div className="flex-1 w-full sm:text-base text-sm">
-          <h1 className="text-center sm:text-3xl text-2xl">What is this?</h1>
+          <h1 className="text-center sm:text-3xl text-2xl">About Wordle Challenge</h1>
           <ul className="list-disc pl-5 block sm:text-base text-sm">
-            <li className="mt-6 mb-2">This is an archive for <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> by <a href="https://twitter.com/powerlanguish">Josh Wardle</a> built on <a href="https://twitter.com/katherinecodes">Katherine Peterson</a>'s <a href="https://octokatherine.github.io/word-master">WordMaster</a></li>
+            <li className="mt-6 mb-2"><a href="https://www.powerlanguage.co.uk/wordle/"><b>Wordle</b></a> is an online daily word guessing game by <a href="https://twitter.com/powerlanguish"><b>Josh Wardle</b>.  </a><a href="https://www.twitter.com/devangvang"><b>Devang Thakkar</b>'s </a><a href="https://www.devangthakkar.com/wordle_archive/"><b>Wordle Archive</b> </a>enables you to play past wordles you may have missed</li>
           </ul>
           <ul className="list-disc pl-5 block sm:text-base text-sm">
-            <li className="mt-6 mb-2">Made with love by <a href="https://www.twitter.com/devangvang">Devang Thakkar</a>.</li>
+            <li className="mt-3 mb-2"><b>Wordle Challenge</b> (made with love by kumarsm and justSahil) takes it further, so you can create and challenge friends with your own wordles!</li>
           </ul>
-          <h1 className="text-center sm:text-3xl text-2xl">How to play?</h1>
+          <h1 className="mt-6 text-center sm:text-3xl text-2xl">How to play?</h1>
           <ul className="list-disc pl-5 block sm:text-base text-sm">
-            <li className="mt-6 mb-2">You have 6 guesses to guess the correct word.</li>
-            <li className="mb-2">You can guess any valid word.</li>
-            <li className="mb-2">{
-              `After each guess, each letter will turn ${colorBlindMode ? 'orange, blue or gray.' : 'green, yellow, or gray.'}`
-            }
-            </li>
+            <li className="mt-6 mb-2">Using only valid words, you have 6 guesses to guess the challenge word. After each guess, each letter will turn:</li>
           </ul>
-          <div className="mb-3 mt-8 flex items-center">
-            <span className={`${colorBlindMode ? 'nm-inset-orange-500' : 'nm-inset-n-green'} text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full`}>
+          <div className="mb-3 mt-3 flex items-center">
+            <span className={`${colorBlindMode ? 'nm-inset-orange-500' : 'nm-inset-n-green'} text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded`}>
               W
             </span>
-            <span className="mx-2">=</span>
-            <span>Correct letter, correct spot</span>
+            <span className="mx-2"></span>
+            <span>{`${colorBlindMode ? 'orange - ' : 'green = '} correct letter, correct position`}</span>
           </div>
           <div className="mb-3">
-            <span className={`${colorBlindMode ? 'nm-inset-blue-300' : 'nm-inset-yellow-500'} text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full`}>
+            <span className={`${colorBlindMode ? 'nm-inset-blue-300' : 'nm-inset-yellow-500'} text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded`}>
               W
             </span>
-            <span className="mx-2">=</span>
-            <span>Correct letter, wrong spot</span>
+            <span className="mx-2"></span>
+            <span>{`${colorBlindMode ? 'blue - ' : 'yellow = '} correct letter, wrong position`}</span>
           </div>
-          <span className="nm-inset-n-gray text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full">
+          <span className="nm-inset-n-gray text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded">
             W
           </span>
-          <span className="mx-2">=</span>
-          <span>Wrong letter</span>
-        </div>
-        <div className="flex justify-center sm:text-base text-sm">
-          <span>This project is open source on</span>
-          <a
-            className="ml-[6px] rounded-full h-5 w-5 sm:h-6 sm:w-6"
-            href="https://github.com/devangthakkar/wordle_archive"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Github />
-          </a>
+          <span className="mx-2"></span>
+          <span>gray =  wrong letter</span>
         </div>
       </div>
     </div>
